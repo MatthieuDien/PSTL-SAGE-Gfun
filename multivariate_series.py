ec918bf4b85b9887c338a65cce54903730555174
Matthieu Dien <matthieu@matthieu-Kubu.(none)>
2013-02-24 19:07:15 +0100

    Useless commit

--- multivariate_series.py
+++ multivariate_series.py
@@ -4,6 +4,13 @@ import sage.structure.parent_base
 
 
 class FormalMultivariatePowerSeriesRing(LazyPowerSeriesRing):
+
+# Inherited method : 
+#  __cmp__(self,x)
+#  _coerce_impl(self,x)
+
+
+
     def __init__(self, F, element_class = None, names = None):
         
         #Use Field to provide inverse method
@@ -15,6 +22,7 @@ class FormalMultivariatePowerSeriesRing(LazyPowerSeriesRing):
         
         self._element_class = element_class if element_class is not None else FormalMultivariatePowerSeries
         self._order = None
+        # _name contains the equivalent of gens
         self._name = names
 
         #TODO : LazyPowerSeriesRing herits from Algebra that use old style parent class
@@ -24,8 +32,13 @@ class FormalMultivariatePowerSeriesRing(LazyPowerSeriesRing):
     def ngens(self):
         return len(self._name)
 
+    def gens(self):
+        return self._name
+
     def __repr__(self):
         return "Formal Multivariate Power Series Ring over %s"%self.base_ring()
+
+    
 
 
 class FormalMultivariatePowerSeries(LazyPowerSeries):
