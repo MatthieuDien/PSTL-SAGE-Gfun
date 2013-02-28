d823f766f6b2045c985348ab4d7137d7fdf5ee5e
DIEN MATTHIEU <2901294@ari-31-303-15.infop6.jussieu.fr>
2013-02-28 10:33:38 +0100

    Added an install script for ARI

---
+++ install_ari.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SAGE_ROOT="/Vrac/sage-5.7"
+SAGE_BIN="$SAGE_ROOT/sage"
+
+echo "Changing the sage's current branch to test"
+$SAGE_BIN -b test
+
+if [[ $? -ne 0 ]]; then
+	echo -e "\nThe test branch doesn't exist"
+	echo "We create it"
+	$SAGE_BIN -b main
+	$SAGE_BIN -clone test
+	$SAGE_BIN -b test
+fi
+
+
+echo -e "\nCopying the files\n"
+cp -v multivariate_series.py all.py $SAGE_ROOT/devel/sage-test/sage/combinat/species/
+
+echo -e "\nCompiling the branch\n"
+$SAGE_BIN -b test
+
+echo -e "\nDone\n"
